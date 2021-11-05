f823f7e928940b2dbef39cae9dc31e42c46c1da6
PeterCore <1550430882@qq.com>
2021-11-06 00:47:28 +0800

    errgroup http start && shutDown

---
+++ go_errgroup/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func StartHttpServer(srv *http.Server) error {
+	http.HandleFunc("/hello", HelloSever)
+	fmt.Println("http server start")
+	err := srv.ListenAndServe()
+	return err
+}
+
+func HelloSever(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "hello, world!\n")
+
+}
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	group, errCtx := errgroup.WithContext(ctx)
+	srv := &http.Server{Addr: ":8080"}
+
+	group.Go(func() error {
+		fmt.Println("http")
+		go func() {
+			<-ctx.Done()
+			fmt.Println("http ctx done")
+			srv.Shutdown(context.TODO())
+		}()
+		return StartHttpServer(srv)
+	})
+
+	signalChanel := make(chan os.Signal, 1)
+	signal.Notify(signalChanel, os.Interrupt, syscall.SIGTERM)
+
+	group.Go(func() error {
+		for {
+			select {
+			case <-errCtx.Done():
+				return errCtx.Err()
+			case <-signalChanel:
+				cancel()
+			}
+		}
+
+	})
+
+	if err := group.Wait(); err != nil {
+		fmt.Println("group error: ", err)
+	}
+	fmt.Println("all group done!")
+
+}
